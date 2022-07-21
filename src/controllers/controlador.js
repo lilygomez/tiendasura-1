5252008cb78c037aec3af1194a708d91d8adf659
lilygomez <lily76371@hotmail.com>
2022-07-21 17:49:54 -0500

    se cambia index y se crea js

---
+++ src/controllers/controlador.js
@@ -0,0 +1,13 @@
+//creando variables
+
+let nombreUsuario="Sophia Salazar"
+const EDAD_USUARIO=12
+let estaturaUsuario=1.55
+let hinchaDelVerde=true
+
+
+console.log(nombreUsuario)
+
+
+console.log(EDAD_USUARIO)
+
